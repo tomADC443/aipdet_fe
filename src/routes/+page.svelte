44eb69a6d0fa7f77bf0e033189c6911b51936b7d
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2024-11-12 12:56:39 +0100

    feat: introduce landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,28 @@
 <script lang="ts">
-	import * as Accordion from '$lib/components/ui/accordion/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
 </script>
 
-<Accordion.Root class="w-full sm:max-w-[70%]">
-	<Accordion.Item value="item-1">
-		<Accordion.Trigger>Is it accessible?</Accordion.Trigger>
-		<Accordion.Content>Yes. It adheres to the WAI-ARIA design pattern.</Accordion.Content>
-	</Accordion.Item>
-	<Accordion.Item value="item-2">
-		<Accordion.Trigger>Is it styled?</Accordion.Trigger>
-		<Accordion.Content>
-			Yes. It comes with default styles that matches the other components' aesthetic.
-		</Accordion.Content>
-	</Accordion.Item>
-	<Accordion.Item value="item-3">
-		<Accordion.Trigger>Is it animated?</Accordion.Trigger>
-		<Accordion.Content>
-			Yes. It's animated by default, but you can disable it if you prefer.
-		</Accordion.Content>
-	</Accordion.Item>
-</Accordion.Root>
+<main class="relative w-full h-screen overflow-hidden">
+	<!-- Background div for SVG -->
+	<div class="absolute inset-0 bg-muted">
+		<!-- Using Tailwind's utility classes to style the SVG as a background image -->
 
-aipdet/src/routes/+page.svelte
+		<div
+			class="h-full w-full bg-no-repeat bg-cover bg-center dark:brightness-[0.7]"
+			style="background-image: url('/src/lib/img/landingBackground.svg');"
+		></div>
+		<a
+			class="absolute right-2 bottom-2 opacity-20 text-xs"
+			href="https://www.vecteezy.com/free-vector/background">Background Vectors by Vecteezy</a
+		>
+	</div>
+	<div class="relative z-10 flex flex-col items-center justify-evenly h-full text-white">
+		<h1 class=" text-4xl font-semibold">
+			Real-Time Invasive Species Detection<br />for Water Resource Management
+		</h1>
+
+		<div>
+			<a href="/auth" class="text-white no-underline"> <Button>Login and try!</Button></a>
+		</div>
+	</div>
+</main>
