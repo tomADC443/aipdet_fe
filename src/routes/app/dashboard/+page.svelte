69edaf949d0e1a1aa9d8e570af6444b9e322092c
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2024-11-18 11:17:48 +0100

    feat: dashboard when logged in

--- src/routes/app/dashboard/+page.svelte
+++ src/routes/app/dashboard/+page.svelte
@@ -1,18 +1,17 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button/index';
+	import NotLoggedIn from '$lib/components/screens/NotLoggedIn.svelte';
+	import { auth0 } from '../../auth/service';
+	import { onMount } from 'svelte';
+
+	let isUserAuthenticated: null | boolean = null;
+
+	onMount(async () => {
+		isUserAuthenticated = await auth0.isAuthenticated();
+	});
 </script>
 
-<div class="flex items-center">
-	<h1 class="text-lg font-semibold md:text-2xl">This is your Dashboard</h1>
-</div>
-<div
-	data-x-chunk-name="dashboard-02-chunk-1"
-	data-x-chunk-description="An empty state showing no products with a heading, description and a call to action to add a product."
-	class="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
->
-	<div class="flex flex-col items-center gap-1 text-center">
-		<h3 class="text-2xl font-bold tracking-tight">You have no products</h3>
-		<p class="text-muted-foreground text-sm">You can start selling as soon as you add a product.</p>
-		<Button class="mt-4">Add Product</Button>
-	</div>
-</div>
+{#if isUserAuthenticated}
+	<h1>you are logged in</h1>
+{:else}
+	<NotLoggedIn title="This is your Dashboard" />
+{/if}
