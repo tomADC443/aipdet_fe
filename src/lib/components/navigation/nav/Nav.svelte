b2537a5fc50db30483ec02f11bb5ebe72faffeb5
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2024-11-18 11:14:09 +0100

    fix: switch mobile nav and nav

--- src/lib/components/navigation/nav/Nav.svelte
+++ src/lib/components/navigation/nav/Nav.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
-	import CircleUser from 'lucide-svelte/icons/circle-user';
-	import Menu from 'lucide-svelte/icons/menu';
+	import Bell from 'lucide-svelte/icons/bell';
 	import Leaf from 'lucide-svelte/icons/leaf';
-	import Search from 'lucide-svelte/icons/search';
+
 	import { page } from '$app/stores';
+
 	import { Button } from '$lib/components/ui/button/index.js';
 	import * as Card from '$lib/components/ui/card/index.js';
-	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
-	import { Input } from '$lib/components/ui/input/index.js';
-	import * as Sheet from '$lib/components/ui/sheet/index.js';
 	import { getNavFromPath } from '../utils';
 	import { navItems } from '../constants';
 
@@ -16,26 +13,25 @@
 	$: route = getNavFromPath($page.url.pathname);
 </script>
 
-<header class="bg-muted/40 flex h-14 items-center gap-4 border-b px-4 lg:h-[60px] lg:px-6">
-	<Sheet.Root>
-		<Sheet.Trigger asChild let:builder>
-			<Button variant="outline" size="icon" class="shrink-0 md:hidden" builders={[builder]}>
-				<Menu class="h-5 w-5" />
-				<span class="sr-only">Toggle navigation menu</span>
+<div class="bg-muted/40 hidden border-r md:block">
+	<div class="flex h-full max-h-screen flex-col gap-2">
+		<div class="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+			<a href="/" class="flex items-center gap-2 font-semibold">
+				<Leaf class="h-6 w-6" />
+				<span class="">SINT</span>
+			</a>
+			<Button variant="outline" size="icon" class="ml-auto h-8 w-8">
+				<Bell class="h-4 w-4" />
+				<span class="sr-only">Toggle notifications</span>
 			</Button>
-		</Sheet.Trigger>
-		<Sheet.Content side="left" class="flex flex-col">
-			<nav class="grid gap-2 text-lg font-medium">
-				<a href="/" class="flex items-center gap-2 text-lg font-semibold">
-					<Leaf class="h-6 w-6" />
-					<span>SINT</span>
-				</a>
-
+		</div>
+		<div class="flex-1">
+			<nav class="grid items-start px-2 text-sm font-medium lg:px-4">
 				{#each navItems as { site, href, icon: Icon, label }}
 					{#if site === route}
 						<a
 							{href}
-							class="bg-muted text-foreground hover:text-foreground mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2"
+							class="bg-muted text-primary hover:text-primary flex items-center gap-3 rounded-lg px-3 py-2 transition-all"
 						>
 							<Icon class="h-5 w-5" />
 							{label}
@@ -43,7 +39,7 @@
 					{:else}
 						<a
 							{href}
-							class="text-muted-foreground hover:text-foreground mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2"
+							class="text-muted-foreground hover:text-primary flex items-center gap-3 rounded-lg px-3 py-2 transition-all"
 						>
 							<Icon class="h-5 w-5" />
 							{label}
@@ -51,47 +47,19 @@
 					{/if}
 				{/each}
 			</nav>
-			<div class="mt-auto">
-				<Card.Root>
-					<Card.Header>
-						<Card.Title>Upgrade to Pro</Card.Title>
-						<Card.Description>
-							Unlock all features and get unlimited access to our support team.
-						</Card.Description>
-					</Card.Header>
-					<Card.Content>
-						<Button size="sm" class="w-full">Upgrade</Button>
-					</Card.Content>
-				</Card.Root>
-			</div>
-		</Sheet.Content>
-	</Sheet.Root>
-	<div class="w-full flex-1">
-		<form>
-			<div class="relative">
-				<Search class="text-muted-foreground absolute left-2.5 top-2.5 h-4 w-4" />
-				<Input
-					type="search"
-					placeholder="Search products..."
-					class="bg-background w-full appearance-none pl-8 shadow-none md:w-2/3 lg:w-1/3"
-				/>
-			</div>
-		</form>
+		</div>
+		<div class="mt-auto p-4">
+			<Card.Root>
+				<Card.Header class="p-2 pt-0 md:p-4">
+					<Card.Title>Upgrade to Pro</Card.Title>
+					<Card.Description>
+						Unlock all features and get unlimited access to our support team.
+					</Card.Description>
+				</Card.Header>
+				<Card.Content class="p-2 pt-0 md:p-4 md:pt-0">
+					<Button size="sm" class="w-full">Upgrade</Button>
+				</Card.Content>
+			</Card.Root>
+		</div>
 	</div>
-	<DropdownMenu.Root>
-		<DropdownMenu.Trigger asChild let:builder>
-			<Button builders={[builder]} variant="secondary" size="icon" class="rounded-full">
-				<CircleUser class="h-5 w-5" />
-				<span class="sr-only">Toggle user menu</span>
-			</Button>
-		</DropdownMenu.Trigger>
-		<DropdownMenu.Content align="end">
-			<DropdownMenu.Label>My Account</DropdownMenu.Label>
-			<DropdownMenu.Separator />
-			<DropdownMenu.Item>Settings</DropdownMenu.Item>
-			<DropdownMenu.Item>Support</DropdownMenu.Item>
-			<DropdownMenu.Separator />
-			<DropdownMenu.Item>Logout</DropdownMenu.Item>
-		</DropdownMenu.Content>
-	</DropdownMenu.Root>
-</header>
+</div>
--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-	import Nav from '$lib/components/navigation/nav/Nav.svelte';
 	import NavMobile from '$lib/components/navigation/nav/NavMobile.svelte';
+	import Nav from '$lib/components/navigation/nav/Nav.svelte';
 	import { ModeWatcher } from 'mode-watcher';
 </script>
 
 <ModeWatcher />
 <div class="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-	<NavMobile />
+	<Nav />
 	<div class="flex flex-col">
-		<Nav />
+		<NavMobile />
 		<main class="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
 			<slot />
 		</main>
