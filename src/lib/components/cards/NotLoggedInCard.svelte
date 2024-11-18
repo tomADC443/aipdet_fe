471d15db93e924e77c69bf998b4ab47dc1d3d9a2
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2024-11-18 11:09:10 +0100

    feat: add not logged in banner

---
+++ src/lib/components/cards/NotLoggedInCard.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
+</script>
+
+<Card.Root>
+	<Card.Header>
+		<Card.Title>Not Logged in</Card.Title>
+		<Card.Description>Unlock all features and process your areas of interest.</Card.Description>
+	</Card.Header>
+	<Card.Content>
+		<a href="/auth"> <Button size="sm" class="w-full">Log-in</Button></a>
+	</Card.Content>
+</Card.Root>
