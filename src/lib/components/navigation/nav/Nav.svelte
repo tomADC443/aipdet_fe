5eeebcd2cd7eb495807095388b3cf0e4cccc8ab9
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2024-11-21 17:49:57 +0100

    chore: change name

--- src/lib/components/navigation/nav/Nav.svelte
+++ src/lib/components/navigation/nav/Nav.svelte
@@ -19,7 +19,7 @@
 		<div class="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
 			<a href="/" class="flex items-center gap-2 font-semibold">
 				<Leaf class="h-6 w-6" />
-				<span class="">SINT</span>
+				<span class="">AIPDET</span>
 			</a>
 			<Button variant="outline" size="icon" class="ml-auto h-8 w-8">
 				<Bell class="h-4 w-4" />
