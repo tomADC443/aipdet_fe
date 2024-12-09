62cf5aa119e45ff1c798e1e067efb284578e0bc0
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2024-12-09 09:14:19 +0100

    feat: add page for tasks

---
+++ src/routes/app/task/create/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { Separator } from '$lib/components/ui/separator';
+	import CreateTask from '$lib/components/forms/CreateTask.svelte';
+</script>
+
+<div class="hidden space-y-6 p-10 pb-16 md:block">
+	<div class="space-y-0.5">
+		<h2 class="text-2xl font-bold tracking-tight">Settings</h2>
+		<p class="text-muted-foreground">Manage your account settings and set e-mail preferences.</p>
+	</div>
+	<Separator class="my-6" />
+	<div class="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+		<CreateTask />
+	</div>
+</div>
