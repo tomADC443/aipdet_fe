44109024a9bec1bad45be006269c51b000cdc432
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2024-12-09 09:10:11 +0100

    feat: make dashboard table to task table

--- src/lib/components/tables/TaskTable.svelte
+++ src/lib/components/tables/TaskTable.svelte
@@ -6,34 +6,12 @@
 	import type { ButtonProps } from '$lib/components/ui/button/index.js';
 	import { Button } from '../ui/button';
 	import { selectedTask } from '#app/stores';
+	import type { Task } from '#routes/app/task/types';
 	function formatUnixTimestampToUTC(unixTimestamp: number): string {
 		const date = new Date(unixTimestamp * 1000);
 		return date.toISOString().split('T')[0];
 	}
-
-	const orders = [
-		{
-			id: '1',
-			name: 'Guam Lake (Egypt)',
-			status: 'Successful',
-			createdAt: 1732038666,
-			isPublic: false
-		},
-		{
-			id: '2',
-			name: 'Lake Tahahm (Tanzania)',
-			status: 'Failed',
-			createdAt: 1732038666,
-			isPublic: true
-		},
-		{
-			id: '3',
-			name: 'Test Region',
-			status: 'In Progress',
-			createdAt: 1732038666,
-			isPublic: true
-		}
-	];
+	export let tasks: Task[] = [];
 
 	function getBadgeVariant(status: string): BadgeVariant {
 		switch (status) {
@@ -61,7 +39,7 @@
 
 <Card.Root
 	data-x-chunk-name="dashboard-05-chunk-3"
-	data-x-chunk-description="A table of recent orders showing the following columns: Customer, Type, Status, Date, and Amount."
+	data-x-chunk-description="A table of recent tasks showing the following columns: Customer, Type, Status, Date, and Amount."
 >
 	<Card.Header class="px-7">
 		<Card.Title>Your Processed Areas</Card.Title>
@@ -80,32 +58,31 @@
 				</Table.Row>
 			</Table.Header>
 			<Table.Body>
-				{#each orders as order}
+				{#each tasks as task}
 					<Table.Row class="odd:bg-accent">
 						<Table.Cell>
-							<div class="font-medium">{order.name}</div>
+							<div class="font-medium">{task.name}</div>
 						</Table.Cell>
 						<Table.Cell>
-							<Badge class="text-xs" variant={getBadgeVariant(order.status)}>{order.status}</Badge>
+							<Badge class="text-xs" variant={getBadgeVariant(task.status)}>{task.status}</Badge>
 						</Table.Cell>
 						<Table.Cell class="hidden md:table-cell"
-							>{formatUnixTimestampToUTC(order.createdAt)}
+							>{formatUnixTimestampToUTC(task.createdAt)}
 						</Table.Cell>
 						<Table.Cell class="hidden sm:table-cell"
-							>{order.isPublic ? 'PUBLIC' : 'PRIVATE'}
+							>{task.isPublic ? 'PUBLIC' : 'PRIVATE'}
 						</Table.Cell>
 						<Table.Cell>
-							{#if $selectedTask && $selectedTask.id === order.id}
+							{#if $selectedTask && $selectedTask.id === task.id}
 								<Button size="sm" variant="secondary">Selected</Button>
 							{:else}
-								<Button size="sm" variant="outline" on:click={handleSelectClick(order.id)}
+								<Button size="sm" variant="outline" on:click={handleSelectClick(task.id)}
 									>Select</Button
 								>
 							{/if}
 						</Table.Cell>
 						<Table.Cell>
-							<Button size="sm" variant={getDeleteButtonVariantByStatus(order.status)}
-								>Delete</Button
+							<Button size="sm" variant={getDeleteButtonVariantByStatus(task.status)}>Delete</Button
 							>
 						</Table.Cell>
 					</Table.Row>
