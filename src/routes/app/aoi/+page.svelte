ca49b4d0be128af8b9d1f0f387b88f2333cf90f1
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2024-12-09 09:12:19 +0100

    feat: add button to create aoi

--- src/routes/app/aoi/+page.svelte
+++ src/routes/app/aoi/+page.svelte
@@ -2,13 +2,8 @@
 	import AoiTable from '$lib/components/tables/AOITable.svelte';
 	import { Button } from '$lib/components/ui/button/index.js';
 	import * as Card from '$lib/components/ui/card/index.js';
-	import { Label } from '$lib/components/ui/label/index.js';
-	import { Textarea } from '$lib/components/ui/textarea/index.js';
-	import LoaderCircle from 'lucide-svelte/icons/loader-circle';
 
-	import { Badge } from '$lib/components/ui/badge/index.js';
-
-	import { Input } from '$lib/components/ui/input/index.ts';
+	import Plus from 'lucide-svelte/icons/plus';
 </script>
 
 <div class="mx-auto grid max-w-[59rem] flex-1 auto-rows-max gap-4">
@@ -21,6 +16,14 @@
 					<Card.Description
 						>Add the name and description of the new Area Of Interest (AOI).</Card.Description
 					>
+					<div class="ml-auto mr-4">
+						<a href="/app/aoi/create">
+							<Button>
+								<Plus class="mr-2 h-4 w-4" />
+								Add AOI
+							</Button>
+						</a>
+					</div>
 				</Card.Header>
 				<Card.Content>
 					<div class="grid gap-6"></div>
