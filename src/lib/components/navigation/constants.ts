fed4e818a74e117d5f8909a6bbf109cab034d8cb
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2025-01-06 12:17:30 +0100

    feat: add task in sidebar

--- src/lib/components/navigation/constants.ts
+++ src/lib/components/navigation/constants.ts
@@ -1,5 +1,6 @@
 import House from 'lucide-svelte/icons/house';
 import LandPlot from 'lucide-svelte/icons/land-plot';
+import Satellite from 'lucide-svelte/icons/satellite';
 
 import ChartArea from 'lucide-svelte/icons/chart-area';
 import TriangleAlert from 'lucide-svelte/icons/triangle-alert';
@@ -15,6 +16,7 @@ type NavItem = {
 export const navItems: NavItem[] = [
 	{ site: 'dashboard', href: '/app/dashboard', icon: House, label: 'Dashboard' },
 	{ site: 'aoi', href: '/app/aoi', icon: LandPlot, label: 'AOIs' },
+	{ site: 'task', href: '/app/task', icon: Satellite, label: 'Tasks' },
 	{ site: 'report', href: '/app/report', icon: ChartArea, label: 'Reports' },
 	{ site: 'alerting', href: '/app/alerting', icon: TriangleAlert, label: 'Alerting' }
 ];
