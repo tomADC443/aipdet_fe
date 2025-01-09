24eeb0b36d5a91b7a011bcee6320574a06144cf0
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2025-01-09 13:32:08 +0100

    feat: make svelte production ready

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,9 @@
-import adapter from '@sveltejs/adapter-node';  // Changed from adapter-auto
+import adapter from '@sveltejs/adapter-node';
 import { sveltePreprocess } from 'svelte-preprocess';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,11 +12,16 @@ const config = {
 	}),
 	kit: {
 		alias: {
-			$lib: '/src/lib',
-			'#routes': './src/routes',
-			'#app': './src/routes/app',
+			$lib: join(dirname(fileURLToPath(import.meta.url)), 'src/lib'),
+			'#routes': join(dirname(fileURLToPath(import.meta.url)), 'src/routes'),
+			'#app': join(dirname(fileURLToPath(import.meta.url)), 'src/routes/app')
 		},
-		adapter: adapter()  // Using node adapter
+		adapter: adapter({
+			// Specific node adapter options
+			out: 'build',
+			precompress: false,
+			envPrefix: ''
+		})
 	}
 };
 
