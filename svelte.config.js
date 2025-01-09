573b5d0aa37de7f9caa95e3f17dfc2aa643e0099
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2025-01-09 13:07:35 +0100

    feat: switch to node adapter (for production)

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';  // Changed from adapter-auto
 import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: sveltePreprocess({
 		postcss: true
 	}),
@@ -13,12 +11,8 @@ const config = {
 			$lib: '/src/lib',
 			'#routes': './src/routes',
 			'#app': './src/routes/app',
-
 		},
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter()  // Using node adapter
 	}
 };
 
