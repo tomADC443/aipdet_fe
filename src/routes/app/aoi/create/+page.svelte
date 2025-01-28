b8043c3c7895ed68d1f66f081d75f01ac2634d04
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2025-01-28 12:48:54 +0100

    feat: add grid

--- src/routes/app/aoi/create/+page.svelte
+++ src/routes/app/aoi/create/+page.svelte
@@ -155,7 +155,7 @@
 	}
 </script>
 
-<div class="mx-auto grid max-w-[59rem] flex-1 auto-rows-max gap-4">
+<div class="grid auto-rows-max items-start gap-4 lg:col-span-2 lg:gap-8">
 	<div class="grid gap-4 md:grid-cols-[1fr_250px] lg:grid-cols-3 lg:gap-8">
 		<!-- <div class="grid auto-rows-max items-start gap-4 lg:col-span-2 lg:gap-8"> -->
 		<form class="grid auto-rows-max items-start gap-4 lg:col-span-2 lg:gap-8">
