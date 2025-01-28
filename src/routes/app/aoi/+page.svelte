cd6a745f095c3812c2e623ea4a80c9191dc01ee9
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2025-01-28 12:48:41 +0100

    refactor: responsive layout

--- src/routes/app/aoi/+page.svelte
+++ src/routes/app/aoi/+page.svelte
@@ -1,35 +1,31 @@
 <script lang="ts">
 	import AoiTable from '$lib/components/tables/AOITable.svelte';
-	import { Button } from '$lib/components/ui/button/index.js';
 	import * as Card from '$lib/components/ui/card/index.js';
-
+	import { Button } from '$lib/components/ui/button';
 	import Plus from 'lucide-svelte/icons/plus';
 </script>
 
-<div class="mx-auto grid max-w-[59rem] flex-1 auto-rows-max gap-4">
-	<div class="grid gap-4 md:grid-cols-[1fr_250px] lg:grid-cols-3 lg:gap-8">
-		<!-- <div class="grid auto-rows-max items-start gap-4 lg:col-span-2 lg:gap-8"> -->
-		<div class="grid auto-rows-max items-start gap-4 lg:col-span-2 lg:gap-8">
-			<Card.Root>
-				<Card.Header>
-					<Card.Title>Description</Card.Title>
+<div class="grid auto-rows-max items-start gap-4 lg:col-span-2 lg:gap-8">
+	<h1 class="text-lg font-semibold md:text-2xl">Areas of Interest (AOIs)</h1>
+	<Card.Root>
+		<Card.Header>
+			<div class=" flex flex-row justify-between items-center pb-6">
+				<div>
+					<Card.Title class="pb-2">Create an Area of Interest</Card.Title>
 					<Card.Description
-						>Add the name and description of the new Area Of Interest (AOI).</Card.Description
+						>In order to perform tasks you first need to register your target area. You can run
+						multiple tasks on the same area.</Card.Description
 					>
-					<div class="ml-auto mr-4">
-						<a href="/app/aoi/create">
-							<Button>
-								<Plus class="mr-2 h-4 w-4" />
-								Add AOI
-							</Button>
-						</a>
-					</div>
-				</Card.Header>
-				<Card.Content>
-					<div class="grid gap-6"></div>
-				</Card.Content>
-			</Card.Root>
-			<AoiTable />
-		</div>
-	</div>
+				</div>
+
+				<a href="/app/aoi/create">
+					<Button class="w-64">
+						<Plus class="mr-2 h-4 w-4" />
+						Add AOI
+					</Button></a
+				>
+			</div>
+		</Card.Header>
+	</Card.Root>
+	<AoiTable />
 </div>
