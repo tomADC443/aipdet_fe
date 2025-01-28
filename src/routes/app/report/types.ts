d6475a1947c6e1860d980b695b30d29c0037ac08
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2025-01-28 14:18:27 +0100

    feat: add type

--- src/routes/app/report/types.ts
+++ src/routes/app/report/types.ts
@@ -3,4 +3,9 @@ export interface NDVIDataPoint {
     value: number;  // Area in square kilometers
 }
 
+export type DashboardFetchData = {
+    status: 'loading' | 'error' | 'success';
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    data: any | null;
+};
 export type NDVIAreaData = NDVIDataPoint[];
