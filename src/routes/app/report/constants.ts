6a3446417a9453b1e027ce122ff1119deb5812f1
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2025-01-28 16:19:28 +0100

    feat: change colors and save opacity

--- src/routes/app/report/constants.ts
+++ src/routes/app/report/constants.ts
@@ -1,9 +1,18 @@
 
 export const COLOR = {
     brightGreenPrimary: "#4CAF50",
-    waterHyacinth: "#FF004D",
-    ndvi: "#00C8FF",
-    observed: "#4CAF50",
-
+    waterHyacinth: {
+        color: "#FF004D",
+        opacity: 0.4,
+    },
+    ndvi: {
+        color: "#00C8FF",
+        opacity: 0.4,
+    },
+    observed: {
+        color: "#4CAF50",
+        opacity: 0.4,
+    },
 
 }
+
