6e7331ffa5812ff144a911ea1f4a82f1c4494300
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2025-01-30 18:45:17 +0100

    feat: forward to app if already loged in

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
 	import authImage from '$lib/img/authimg_small.jpg';
-	const loginLink = `${import.meta.env.VITE_BASE_URL}/auth/login`;
-	const signupLink = `${import.meta.env.VITE_BASE_URL}/auth/signup`;
+	import { isLoggedIn } from '#app/stores';
+	import { goto } from '$app/navigation';
+
+	if ($isLoggedIn) {
+		goto('/app/task');
+	}
 </script>
 
 <div class="flex justify-center lg:flex-row items-stretch w-full min-h-screen">
@@ -12,12 +16,11 @@
 			<p class="text-muted-foreground text-balance">Log-in or Sign-up to access the app</p>
 		</div>
 		<div class="gap-4 min-w-80">
-			<a href={loginLink}><Button class="w-full">Login</Button></a>
+			<a href="/auth/login"><Button class="w-full">Login</Button></a>
 		</div>
 		<div class="gap-4 min-w-80">
-			<a href={signupLink}><Button class="w-full">Sign-up</Button></a>
+			<a href="/auth/signup"><Button class="w-full">Sign-up</Button></a>
 		</div>
-		<div class="mt-4 text-sm">You will be redirected to the Authentication Provider</div>
 	</div>
 	<div class="hidden bg-muted lg:flex lg:basis-1/2">
 		<img
