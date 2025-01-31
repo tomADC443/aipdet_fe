32710edb6fe088cf0022cc0afef9ab332850022e
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2025-01-31 13:32:53 +0100

    feat: add public table, abstract table from container logic

--- src/routes/app/task/+page.svelte
+++ src/routes/app/task/+page.svelte
@@ -1,58 +1,16 @@
 <script lang="ts">
-	import TaskTable from '$lib/components/tables/TaskTable.svelte';
+	import TaskTable from '#routes/app/task/TaskTable.svelte';
 	import { Button } from '$lib/components/ui/button/index.js';
 	import * as Card from '$lib/components/ui/card/index.js';
-	import type { Task } from '#app/task/types';
 	import Plus from 'lucide-svelte/icons/plus';
-	import toast from 'svelte-french-toast';
-	import { onMount } from 'svelte';
-
-	import { Content } from '$lib/components/ui/accordion';
-
-	onMount(() => {
-		fetchTasks();
-	});
-
-	let tasks: Task[] = [];
-	let status: 'loading' | 'success' | 'empty' | 'error' = 'loading';
-	async function fetchTasks() {
-		status = 'loading';
-		try {
-			const response = await fetch(import.meta.env.VITE_BASE_URL_API + '/api/tasks', {
-				credentials: 'include',
-				headers: {
-					'Content-Type': 'application/json',
-					Connection: 'keep-alive'
-				}
-			});
-			const data = await response.json();
-
-			if (response.ok) {
-				tasks = data;
-				if (tasks.length === 0) {
-					status = 'empty';
-				} else {
-					status = 'success';
-				}
-				return;
-			} else {
-				status = 'error';
-				toast.error('Failed to get Tasks. Try again Later.');
-				console.error('Response not ok:', response.body);
-			}
-		} catch (error) {
-			status = 'error';
-			toast.error('Failed to get Tasks. Try again Later.');
-			console.error('Error fetching Tasks:', error);
-		}
-	}
+	import PublicTaskTable from './PublicTaskTable.svelte';
 </script>
 
 <div class="grid auto-rows-max items-start gap-4 lg:col-span-2 lg:gap-8 flex-wrap">
 	<h1 class="text-lg font-semibold md:text-2xl">Tasks</h1>
 	<Card.Root>
 		<Card.Header>
-			<div class=" flex flex-row justify-between items-center pb-6">
+			<div class=" flex flex-row justify-between items-center gap-4 pb-6">
 				<div>
 					<Card.Title class="pb-2">Create a Task</Card.Title>
 					<Card.Description>
@@ -62,15 +20,19 @@
 					</Card.Description>
 				</div>
 
-				<a href="/app/aoi/create">
+				<a href="/app/task/create">
 					<Button class="w-64">
 						<Plus class="mr-2 h-4 w-4" />
-						Add AOI
+						Add Task
 					</Button></a
 				>
 			</div>
 		</Card.Header>
 	</Card.Root>
+	<h1 class="text-lg font-semibold md:text-2xl">Your Tasks</h1>
+	<TaskTable />
+	<h1 class="text-lg font-semibold md:text-2xl">Public Tasks of other Users</h1>
+	<PublicTaskTable />
 
-	<TaskTable {tasks} />
+	<!-- <TaskTable tasks={publicTasks} /> -->
 </div>
