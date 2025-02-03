cf164232e36704ece6b0733ca9becea2b700fc47
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2025-02-03 11:17:40 +0100

    fix: remove data-x descriptions

--- src/lib/components/screens/NotLoggedIn.svelte
+++ src/lib/components/screens/NotLoggedIn.svelte
@@ -7,11 +7,7 @@
 <div class="flex items-center">
 	<h1 class="text-lg font-semibold md:text-2xl">{title}</h1>
 </div>
-<div
-	data-x-chunk-name="dashboard-02-chunk-1"
-	data-x-chunk-description="An empty state showing no products with a heading, description and a call to action to add a product."
-	class="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
->
+<div class="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm">
 	<div class="flex flex-col items-center gap-1 text-center">
 		<h3 class="text-2xl font-bold tracking-tight">You are not logged in</h3>
 		<p class="text-muted-foreground text-sm">Please login first to view this feature.</p>
--- src/routes/app/report/+page.svelte
+++ src/routes/app/report/+page.svelte
@@ -43,11 +43,7 @@
 	<div class="flex items-center">
 		<h1 class="text-lg font-semibold md:text-2xl">Reportings</h1>
 	</div>
-	<div
-		data-x-chunk-name="dashboard-02-chunk-1"
-		data-x-chunk-description="An empty state showing no products with a heading, description and a call to action to add a product."
-		class="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
-	>
+	<div class="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm">
 		<div class="flex flex-col items-center gap-1 text-center">
 			<h3 class="text-2xl font-bold tracking-tight">You have no Task selected</h3>
 			<p class="text-muted-foreground text-sm">Select a Task to view the Reports here.</p>
