a0c71fa9ffd8d7f401db001aaca877bc866ed05d
tomADC443 <61840572+tomADC443@users.noreply.github.com>
2025-02-05 16:22:01 +0100

    fix: wrong for label

--- src/routes/auth/signup/+page.svelte
+++ src/routes/auth/signup/+page.svelte
@@ -109,7 +109,7 @@
 			<div class="grid gap-4">
 				<div class="grid grid-cols-2 gap-4">
 					<div class="grid gap-2">
-						<Label for="last-name">First name</Label>
+						<Label for="first-name">First name</Label>
 						<Input
 							id="first-name"
 							placeholder="John"
